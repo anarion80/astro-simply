da621108cea04d72989114a66074520fa02f24d8
Anarion <anarion80@gmail.com>
2024-09-21 20:28:26 +0200

    :sparkles: Update medium zoom script for view transitions

--- src/utils/mediumZoom.ts
+++
@@ -1,8 +0,0 @@
-import mediumZoom from 'medium-zoom';
-
-export default function mediumZoomImg(): void {
-    mediumZoom('.simply-zoom', {
-        margin: 20,
-        background: `hsla(0,0%,100%,.85)`,
-    });
-}
